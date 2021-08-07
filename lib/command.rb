1c2c53440f463ca39c1c8dc2929fa7f55dd2b868
Caleb Rainey <caleb@rainey.cc>
2021-08-07 10:53:52 -0400

    refactor to keep code in command class

--- lib/command.rb
+++ lib/command.rb
@@ -77,4 +77,33 @@ class Command
     return text
   end
 
+  def self.create_channel(event)
+    args = event.content.split(" ")
+    if args[0] == "!createchannel"
+      if args[1] != nil
+        name = args[1]
+        if args[2] != nil
+          if args[2] == "private"
+            role = event.server.roles.find { |r| r.name == '@everyone' }
+            overwrites = []
+            overwrites << Discordrb::Overwrite.new(role.id, type: 'role', allow: 0, deny: 1024)
+            overwrites << Discordrb::Overwrite.new(event.message.author.id, type: "member", allow: 139653860417, deny: 0)
+            event.message.mentions.each do |u|
+              overwrites << Discordrb::Overwrite.new(u.id, type: "member", allow: 139653860417, deny: 0)
+            end
+            channel = event.server.create_channel(name, parent: event.channel.parent_id, permission_overwrites: overwrites)
+          else
+            channel = event.server.create_channel(name, parent: event.channel.parent_id)
+          end
+        else
+          channel = event.server.create_channel(name, parent: event.channel.parent_id)
+        end
+      else
+        event.respond "Error: I need a channel name to create one!"
+      end
+    else
+      event.respond "Error: Invalid channel creation command."
+    end
+  end
+
 end
